cc5122931e2211f8403f33d32f3e683cde2ad3c6
Melissa Lobo <mellobomel@gmail.com>
2016-01-19 19:04:25 -0200

    Adicionando model Midia e Category

---
+++ src/main/java/model/Category.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum Category {
+
+	BOOKS, DOCUMENTS, MUSIC, PICTURES, VIDEOS
+}
---
+++ src/main/java/model/Media.java
@@ -0,0 +1,56 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Media {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nameArquivo;
+	private String arquivo;
+	private String tag;
+	private Category category;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNameArquivo() {
+		return nameArquivo;
+	}
+	public void setNameArquivo(String nameArquivo) {
+		this.nameArquivo = nameArquivo;
+	}
+	public String getArquivo() {
+		return arquivo;
+	}
+	public void setArquivo(String arquivo) {
+		this.arquivo = arquivo;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	@Override
+	public String toString() {
+		return "Media [id=" + id + ", nameArquivo=" + nameArquivo + ", arquivo=" + arquivo + ", tag=" + tag
+				+ ", category=" + category + "]";
+	}
+	
+	
+}
