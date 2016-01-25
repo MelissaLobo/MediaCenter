9e32e98a414503cdb15f218cbcce2f7396bf7c2c
Melissa Lobo <mellobomel@gmail.com>
2016-01-25 05:54:50 -0200

    Adicionando Metodos

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,5 +1,9 @@
 package controller;
 
+/**
+ * @author MelissaLobo mellobomel@gmail.com
+ */
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,9 @@
 package controller;
 
+/**
+ * @author MelissaLobo mellobomel@gmail.com
+ */
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
