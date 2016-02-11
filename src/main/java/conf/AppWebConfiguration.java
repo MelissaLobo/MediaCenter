45a67f25ed0196d414f12287a11b7f1a90244fbd
Melissa Lobo <mellobomel@gmail.com>
2016-02-11 00:39:29 -0200

    Configuração data

--- src/main/java/conf/AppWebConfiguration.java
+++ src/main/java/conf/AppWebConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -46,4 +50,16 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter{
 
         return messageSource;
     }
+	
+	//Config Data 
+	@Bean
+	public FormattingConversionService mvcConversionService(){
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+		registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
+		registrar.registerFormatters(conversionService);
+		
+		return conversionService;
+		
+	}
 }
