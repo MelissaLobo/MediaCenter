77ab9badf9399420c7937c4a177da1b759574703
Melissa Lobo <mellobomel@gmail.com>
2016-03-08 23:15:45 -0300

    configure Global Security

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,13 +1,20 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author MelissaLobo mellobomel@gmail.com
  */
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import dao.UserDao;
 import model.User;
@@ -38,8 +45,16 @@ public class UserController {
 		return "login";
 	}
 	
-	@RequestMapping("/logout")
-	public String showLogout(){
-		return "logout";
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";
+	    //É uma boa pratica redirecionar para a pagina de login novamente
+
 	}
+	
+	
 }
